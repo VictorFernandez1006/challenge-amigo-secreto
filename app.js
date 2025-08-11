0d229e73f5b0d16d752ad0755c9bffc0f7350a55
Victor Fernández <victor.fernandez1006@gmail.com>
2025-08-11 16:39:29 -0500

    Actualizar lista de amigos

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ let arregloNombres = [];
 function agregarAmigo(){
     let obtenerNombre = document.querySelector('.input-name').value;
     arregloNombres.push(obtenerNombre);
+    actualizarAmigos(arregloNombres)
     limpiarInput();
 }
 function limpiarInput(){
+    arregloNombres = [];
     document.querySelector('.input-name').value = '';
 }
 
+function actualizarAmigos(){
+    let elementoLista = document.querySelector('.name-list');
+    arregloNombres.forEach(elemento => {
+    let item = document.createElement('li');
+    item.textContent = elemento;
+    elementoLista.appendChild(item);
+    });
+
+}
 
 
 
