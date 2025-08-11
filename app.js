1d0a8d3834c87ffa73442a38b87a751562eee46b
Victor Fernández <victor.fernandez1006@gmail.com>
2025-08-11 16:20:46 -0500

    Método para agregar amigos

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // Creación de arreglo que va a almacenar los nombres.
 // Se declara global para que las funciones tengan acceso.
-let arregloNombre = [];
+let arregloNombres = [];
+
+
+function agregarAmigo(){
+    let obtenerNombre = document.querySelector('.input-name').value;
+    arregloNombres.push(obtenerNombre);
+    limpiarInput();
+}
+function limpiarInput(){
+    document.querySelector('.input-name').value = '';
+}
+
+
+
+
