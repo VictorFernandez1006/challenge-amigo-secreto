6bc6453b1502ba4d9fbe92b97fe90e743bff52ce
Victor Fernández <victor.fernandez1006@gmail.com>
2025-08-11 15:58:12 -0500

    Creción de arreglo para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Creación de arreglo que va a almacenar los nombres.
+// Se declara global para que las funciones tengan acceso.
+let arregloNombre = [];
