109968348167699aeddbce37b51754a9d7558ac7
Victor Fernández <victor.fernandez1006@gmail.com>
2025-08-11 18:39:00 -0500

    Se agvrega funcionalidad para el sorteo de amigos

--- app.js
+++ app.js
@@ -4,25 +4,36 @@ let arregloNombres = [];
 
 
 function agregarAmigo(){
-    let obtenerNombre = document.querySelector('.input-name').value;
-    arregloNombres.push(obtenerNombre);
-    actualizarAmigos(arregloNombres)
+    let obtenerNombre = document.querySelector('.input-name').value.trim();
+    if (!arregloNombres.includes(obtenerNombre)){
+        arregloNombres.push(obtenerNombre);
+        actualizarAmigos(arregloNombres)
+    }
     limpiarInput();
+
 }
 function limpiarInput(){
-    arregloNombres = [];
     document.querySelector('.input-name').value = '';
 }
 
 function actualizarAmigos(){
     let elementoLista = document.querySelector('.name-list');
+    elementoLista.innerHTML ='';
     arregloNombres.forEach(elemento => {
     let item = document.createElement('li');
     item.textContent = elemento;
     elementoLista.appendChild(item);
     });
-
 }
+function sortearAmigo(){
+    let numeroSorteado = Math.floor(Math.random() * arregloNombres.length)
+    let amigoSorteado = document.querySelector('.result-list');
+    amigoSorteado.innerHTML = '';
+    let elemento = document.createElement('li');
+    elemento.textContent = arregloNombres[numeroSorteado];
+    amigoSorteado.appendChild(elemento);
+}
+
 
 
 
